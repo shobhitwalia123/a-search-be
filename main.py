09942cb5799e1c2d16cef0e40909e773289186cf
shobhit-0612 <shobhit@schnellsoftsolutions.com>
2025-09-25 10:32:39 +0530

    build fix

--- main.py
+++
@@ -1,7 +0,0 @@
-# Main FastAPI entrypoint for deployment
-from api.index import app
-
-# Export the app for deployment systems
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
